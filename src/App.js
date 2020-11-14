848b8dd654fcad43205b682dddc7f388e85f31c8
IsaacGHoward <howardisaac100@gmail.com>
2020-11-13 16:55:44 -0800

    Set up some basic routing between pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+import ClientView from './ClientView'
+import InstructorView from './InstructorView'
+import CreateClass from './CreateClass'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/">
+        <div className="App">
+          <header className="App-header">
+            Main
+          </header>
+        </div>
+      </Route>
+      <Route path="/client">
+        <ClientView/>
+      </Route>
+      <Route path="/instructor">
+        <InstructorView/>
+      </Route>
+      <Route path="/createclass">
+        <CreateClass/>
+      </Route>
+    </Router>
+    
   );
 }
 
