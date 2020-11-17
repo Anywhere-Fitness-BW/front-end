b9994ed3de9f9396caf326986d2ced5f7e3b3fec
IsaacGHoward <howardisaac100@gmail.com>
2020-11-16 17:04:31 -0800

    Reset project files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import Home from './Home';
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,52 +8,11 @@ import {
   useParams
 } from "react-router-dom";
 
-// import ClientView from './ClientView'
-// import InstructorView from './InstructorView'
-// import CreateClass from './CreateClass'
-
 
 function App() {
   return (
 
-    <Router>
-      {/* <Route path="/">
-        <div className="navBar">
-          Logged in as : Username | Role | <Link to={'/'}>Back to home</Link>
-          
-        </div>
-      </Route>
-      <Route exact path="/">
-        <div className="App">
-          <div className="App-header">
-            Main
-           
-          </div>
-          
-        </div>
-      </Route>
-      <Route path="/client">
-        <ClientView/>
-      </Route>
-      <Route path="/instructor">
-        <InstructorView/>
-      </Route>
-      <Route path="/createclass">
-        <CreateClass/>
-      </Route> */}
-     <div>
-     <Link to='/Home'>
-                <button>Register</button>
-           </Link>
-      <Switch>
-          <Route exact path="/Home" component={Home} >
-            
-            
-            </Route>
-        </Switch>
-        </div>
-     
-    </Router>
+   <h3>Test</h3>
     
   );
 }
