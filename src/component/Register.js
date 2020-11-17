3c0a322517341ed9727972048a465b3109be53b7
IsaacGHoward <howardisaac100@gmail.com>
2020-11-16 22:41:46 -0800

    Turned register into stateless component

--- src/component/Register.js
+++ src/component/Register.js
@@ -2,16 +2,16 @@ import React, {Component} from 'react';
 
 import axios from 'axios';
 
-export default class Register extends Component {
-
-    handleSubmit = e => {
+export default function Register(){
+    /*
+    const handleSubmit = e => {
         e.preventDefault();
         const data = {
             first_name: this.firstName,
             last_name: this.lastName,
             email: this.email,
             password: this.password,
-          password_confirm: this.confirmPassword
+            password_confirm: this.confirmPassword
         };
      console.log(data);
     // axios.post('https://localhost:8000/register', data).then(
@@ -24,46 +24,44 @@ export default class Register extends Component {
     //     }
     // )
 
-    };
-    render(){
+    };*/
+    
         return (
-           <form onSubmit={this.handleSubmit}>
+           <form>
                <h3>Sign Up</h3>
                <div className='form-group'>
                    <label>First Name</label>
                    <input type='text' className='form-control'
                    placeholder='First Name'
-                   onChange={e => this.firstName = e.target.value} />
+                   />
                </div>
 
                <div className="form-group">
                    <label>Last Name</label>
                    <input type='text' className='form-control'
                    placeholder='Last Name' 
-                   onChange={e => this.lastName = e.target.value} />
+                   />
                </div>
                <div className='form-group'>
                    <label>Email</label>
                    <input type='email' className='form-control'
                    placeholder='email'
-                   onChange={e => this.email = e.target.value} />
+                   />
                </div>
                <div className='form-group'>
                    <label>Password</label>
                    <input type='password' className='form-control'
                    placeholder='Password'
-                   onChange={e => this.password = e.target.value} />
+                   />
                </div>
                <div className='form-group'>
                    <label>Conform Password</label>
                    <input type='password' className='form-control'
                    placeholder='Conform Password'
-                   onChange={e => this.conformPassword = e.target.value} />
-                   
+                   />
                </div>
                
                <button className="btn btn-primary btn-block">Sign Up</button>
            </form>
         )
     }
-}
