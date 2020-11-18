7f3c3509824ec3c9fdc286d70f190fa3d2357f69
IsaacGHoward <howardisaac100@gmail.com>
2020-11-17 16:54:17 -0800

    Converted the file to stateless function, changed email to username

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,8 +1,9 @@
 import Axios from 'axios';
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-export default class Login extends Component {
+export default function Login() {
+    /*
     handleSubmit = (e) => {
         e.preventDefault();
 
@@ -20,26 +21,25 @@ export default class Login extends Component {
         //     console.log(err)
         // })
     };
-    render(){
+    */
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form>
                <h3>Login</h3>
               
                <div className='form-group'>
-                   <label>Email</label>
-                   <input type='email' className='form-control'
-                   placeholder='email'
-                   onChange={e => this.email = e.target.value} />
+                   <label>Username</label>
+                   <input type='text' className='form-control'
+                   placeholder='Username'
+                    />
                </div>
                <div className='form-group'>
                    <label>Password</label>
                    <input type='password' className='form-control'
                    placeholder='Password'
-                   onChange={e => this.password = e.target.value} />
+                   />
                </div>
                
                <button className="btn btn-primary btn-block">Login</button>
            </form>
         )
     }
-}
