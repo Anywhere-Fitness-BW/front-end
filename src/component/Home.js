3b2bd1d839444e0ce9ed2014b6cc029af8212344
IsaacGHoward <howardisaac100@gmail.com>
2020-11-19 16:49:36 -0800

    Made home screen nicer looking

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,7 +3,11 @@ import React, {Component} from 'react';
 export default class Home extends Component {
     render(){
         return (
-            <h2>You are not logged in</h2>
+            <div style={{textAlign: "center"}}>
+                <h2>Welcome to Anywhere Fitness!</h2>
+                <h5>Please login or sign up to continue</h5>
+            </div>
+            
         )
     }
 }
